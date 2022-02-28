f848388e75d943c9fbf7c70d39e3ed483f72e215
Jae-cisco <76939023+Jae-cisco@users.noreply.github.com>
2022-02-28 16:14:45 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+ region = "ap-northeast-2"
+ access_key = "AKIA4NEZNKNXLFGSHNOM"
+ secret_key = "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"
+}
+
+resource "aws_instance" "helloworld" {
+  ami = "ami-0454bb2fefc7de53"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Ubuntu"
+  }
+}
